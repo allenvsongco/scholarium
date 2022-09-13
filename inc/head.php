74e1cfdf58c8766b9ab13af83f41d96a890fded7
Repz Lopez <repzlopez@gmail.com>
2022-09-14 02:12:07 +0800

    updated logo added favicon

--- inc/head.php
+++ inc/head.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo SCLR_FULL . $title; ?></title>
+    <link rel="icon" type="image/x-icon" href="/assets/images/favicon.ico">
     <link rel="stylesheet" href="<?php echo $root; ?>assets/css/styles.css">
     <?php if (isset($addcss) && $addcss != '') echo '<link rel="stylesheet" href="' . $root . 'assets/css/' . $addcss . '.css">'; ?>
 </head>
-
